0fc15474aba23d617fb1221715302e186ae6e214
pauls <paul.saguez@etud.u-picardie.fr>
2026-01-22 17:41:17 +0100

    Mise en place architecture et documentation

---
+++ API/Models/MigrationReport.cs
@@ -0,0 +1,52 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace API.Models
+{
+    public class MigrationReport
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("migration_start_time")]
+        public string? MigrationStartTime { get; set; }
+
+        [BsonElement("sub_job_id")]
+        public string? SubJobId { get; set; }
+
+        [BsonElement("title")]
+        public string? Title { get; set; }
+
+        [BsonElement("type")]
+        public string? Type { get; set; }
+
+        [BsonElement("source_id")]
+        public string? SourceId { get; set; }
+
+        [BsonElement("source")]
+        public string? Source { get; set; }
+
+        [BsonElement("destination_id")]
+        public string? DestinationId { get; set; }
+
+        [BsonElement("destination")]
+        public string? Destination { get; set; }
+
+        [BsonElement("size")]
+        public string? Size { get; set; }
+
+        [BsonElement("status")]
+        public string? Status { get; set; }
+
+        [BsonElement("migration_action")]
+        public string? MigrationAction { get; set; }
+
+        [BsonElement("comment")]
+        public string? Comment { get; set; }
+
+        [BsonElement("error_code")]
+        public string? ErrorCode { get; set; }
+    }
+}
